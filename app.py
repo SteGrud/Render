3c29220decee5a252fd485169a7717cb129c0cf8
SteGrud <stephanie.grudzinski@hotmail.com>
2025-01-17 12:46:58 -0300

    app.py

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-import streamlit as st
-
-st.header('Jogando uma moeda')
-
-number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
-start_button = st.button('Executar')
-
-if start_button:
-    st.write(f'Executando o experimento de {number_of_trials} tentativas.')
-
-st.write('Ainda não é um aplicativo funcional. Em construção.')
-
 import scipy.stats
 import streamlit as st
 import time
